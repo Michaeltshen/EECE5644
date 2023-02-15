fd7a9cb460e54d7c1ee1687e5e4799d10c321ffa
michaeltshen <shen.mich@northeastern.edu>
2023-02-15 18:34:45 -0500

    Final push of HW2

--- HW2/Question1/HW2_1B.py
+++ HW2/Question1/HW2_1B.py
@@ -7,12 +7,12 @@ from matplotlib.lines import Line2D
 
 
 def fisher(data):
-    label_0, label_1 = [], []
     false_positive_list, true_positive_list, error_rate_list = [], [], []
     tau = -10
     # Calculate LDA Fisher stuff
     while tau < 10:
         true_negative, false_negative, false_positive, true_positive = 0, 0, 0, 0
+        label_0, label_1 = [], []
         for num in range(0, len(data)):
             value = data.iloc[num].to_numpy()[:-1]
             if data["class_prior"][num] == 0:
@@ -31,7 +31,6 @@ def fisher(data):
             mu = m0 - m1
             w = np.matmul(np.linalg.inv(Sw), mu.T)
             classifier = np.matmul(w.T, value)
-            # print(classifier)
 
             if classifier > tau:
                 decision = 1
@@ -57,8 +56,7 @@ def fisher(data):
         true_positive_list.append(true_positive_rate)
         false_positive_list.append(false_positive_rate)
         error_rate_list.append(error_rate)
-        print(tau)
-        tau += 0.25
+        tau += 0.1
 
     min_error_rate = 100000000
     min_index = 0
@@ -68,7 +66,7 @@ def fisher(data):
             min_index = num
 
     print("The estimated minimum error rate is : " + str(min_error_rate))
-    print("The tau value at that point is: " + str(min_index - 10))
+    print("The tau value at that point is: " + str((min_index*0.1) - 10))
 
     fig, ax = plt.subplots(1, 1, figsize=(5, 5))
     ax.plot(false_positive_list, true_positive_list, 'b.', markersize=2)
@@ -83,7 +81,7 @@ def fisher(data):
     labels = ['Estimated Optimal Point']
 
     # # Put a legend below current axis
-    ax.legend(handles=legend_elements, labels=labels, bbox_to_anchor=(0.65, 0.2),
+    ax.legend(handles=legend_elements, labels=labels, bbox_to_anchor=(0.3, 1),
               loc='upper center', fancybox=True)
     plt.savefig("1B.png")
 
